76fbd68eb646935e7c36b2ce315d15eaf928eae8
Farel Farizi P <farel.purnomosasi20@gmail.com>
2026-04-30 10:43:24 +0700

    migrasi database

---
+++ database/migrations/2026_04_30_020308_create_sub_wilayah_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_wilayah', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('wilayah_id')->constrained('wilayah');
+            $table->string('nama_sub_wilayah');
+            $table->string('kode_wilayah')->unique();
+            $table->bool('materi_1_aktif')->default(false);
+            $table->bool('materi_2_aktif')->default(false);
+            $table->bool('materi_3_aktif')->default(false);
+            $table->bool('materi_4_aktif')->default(false);
+            $table->bool('materi_5_aktif')->default(false);
+            $table->bool('materi_6_aktif')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_wilayah');
+    }
+};
